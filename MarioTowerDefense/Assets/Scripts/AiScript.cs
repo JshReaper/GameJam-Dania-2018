c45167593a8f4e18c28a8f0754bc7ccfdf1a4935
Jacob SH <jshchansen@gmail.com>
2018-04-19 12:10:23 +0200

    added test scene

---
+++ MarioTowerDefense/Assets/Scripts/AiScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AiScript : MonoBehaviour {
+    NavMeshAgent agent;
+    [SerializeField]
+    GameObject target;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
